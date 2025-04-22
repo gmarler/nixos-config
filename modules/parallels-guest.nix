0c95ce302e1bd2c7dfba4885fc5139d0f8c8f550
Gordon Marler <gmarler@bloomberg.net>
2025-04-22 12:56:57 -0400

    Turn on timesyncd service

--- modules/parallels-guest.nix
+++ modules/parallels-guest.nix
@@ -54,7 +54,7 @@ in
 
     boot.kernelModules = [ "prl_fs" "prl_fs_freeze" "prl_tg" ] ++ optional aarch64 "prl_notifier";
 
-    # services.timesyncd.enable = false;
+    services.timesyncd.enable = true;
 
     systemd.services.prltoolsd = {
       description = "Parallels Tools' service";
